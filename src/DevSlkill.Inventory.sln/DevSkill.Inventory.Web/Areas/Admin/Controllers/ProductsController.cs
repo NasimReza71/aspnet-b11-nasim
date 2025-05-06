f667d341ad1dd5cd008e1d22b5c9f3aabe4a2a8e
Nasim Reza <nasim.reza201@gmail.com>
2025-05-06 13:12:53 +0600

    Book Controllers/ProductsController.cs,AddProductModel.cs,Add.cshtml,form added from adminlte,http,validateantiforgery token added,Areas("Admin")) added.

---
+++ src/DevSlkill.Inventory.sln/DevSkill.Inventory.Web/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using DevSkill.Inventory.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevSkill.Inventory.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            var model = new AddProductModel();
+            return View(model); 
+        }
+
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Add(AddProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+            }
+
+           
+            return View(model);
+        }
+
+    }
+}
---
+++ src/DevSlkill.Inventory.sln/DevSkill.Inventory.Web/Areas/Admin/Models/AddProductModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DevSkill.Inventory.Web.Areas.Admin.Models
+{
+    public class AddProductModel
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime ManufactureDate { get; set; }
+    }
+}
