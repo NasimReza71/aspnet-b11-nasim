81a521d1c14c23de2c85665d27b972a78cf4abeb
Nasim Reza <nasim.reza201@gmail.com>
2025-05-27 20:36:53 +0600

    Data Annotation implemented

--- src/DevSkill.Inventory.sln/DevSkill.Inventory.Web/Areas/Admin/Models/AddProductModel.cs
+++ src/DevSkill.Inventory.sln/DevSkill.Inventory.Web/Areas/Admin/Models/AddProductModel.cs
@@ -1,13 +1,17 @@
-﻿namespace DevSkill.Inventory.Web.Areas.Admin.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSkill.Inventory.Web.Areas.Admin.Models
 {
     public class AddProductModel
     {
+        [Required, MaxLength(100)]
         public string Name { get; set; }
 
         public double Price { get; set; }
 
         public string? Description { get; set; }
 
+        [Required]
         public DateTime? ManufactureDate { get; set; }
     }
 }
